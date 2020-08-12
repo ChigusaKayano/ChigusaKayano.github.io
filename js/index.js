f91c268b94af8038c8e8d5ce68913d934acbfd7f
Florian Lepage <Chigusa.Kayano@outlook.com>
2020-08-12 15:54:06 +0200

    Add central image
    Add an image that is located at the cneter of the website

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@ const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll('.nav-links li');
-
     burger.addEventListener("click", () => {
         //toggle Nav
         nav.classList.toggle("nav-active");
